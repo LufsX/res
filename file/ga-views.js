1a310cba049df5720b4da9121ab19b3c1fc2a6d7
Lufsx <lufs00@outlook.com>
2020-06-27 13:59:06 +0800

    add: ga-views.min.js

--- file/ga-views.js
+++ file/ga-views.js
@@ -1,1 +1,19 @@
-(async()=>{function GetUrlRelativePath(){var url=document.location.toString();var arrUrl=url.split("//");var start=arrUrl[1].indexOf("/");var relUrl=arrUrl[1].substring(start);if(relUrl.indexOf("?")!=-1){relUrl=relUrl.split("?")[0]}return relUrl}var obj=document.getElementById("pv-counter");var show=document.getElementById("post-meta-views");const query=GetUrlRelativePath();const json=await fetch(`${window.post_views_api}?page=${query}`).then((res)=>res.json());const hit=json[0].hit;if(hit!==undefined&&show!==null){obj.innerHTML=`${hit}`;show.hidden=false}})();
+(async () => {
+  var obj = document.getElementById("pv-counter");
+  var show = document.getElementById("post-meta-views");
+  var url = document.location.toString();
+  var arrUrl = url.split("//");
+  var start = arrUrl[1].indexOf("/");
+  var relUrl = arrUrl[1].substring(start);
+  if (relUrl.indexOf("?") != -1) {
+    relUrl = relUrl.split("?")[0];
+  }
+  const json = await fetch(
+    `${window.post_views_api}?page=${relUrl}`
+  ).then((res) => res.json());
+  const hit = json[0].hit;
+  if (hit !== undefined && show !== null) {
+    obj.innerHTML = `${hit}`;
+    show.hidden = false;
+  }
+})();
